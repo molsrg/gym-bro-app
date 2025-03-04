9f2b100738f57fa87a423b7fa6300fca34153bf8
molsrg <molsrg.35@gmail.com>
2025-03-04 14:09:44 +0300

    feat(ui): add alert hint in home page #10

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { CheckDayActivity, DayActivityModal, ScheduleTrainingModal } from '~/components/modal'
 
+import { useTrainingInfo } from '~/constants/app/questions'
 import {
   extractDateComponents,
   findDateDifference,
@@ -10,6 +11,7 @@ import {
 
 const slideover = useSlideover()
 const modal = useModal()
+const { typesTraining } = useTrainingInfo()
 // TODO: remove this when the API is ready
 onMounted(async () => {
   useActiveStore().toggleLoadingCalendar()
@@ -41,15 +43,38 @@ async function openDayActivityModal(date) {
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center gap-3.5">
-    <UAlert
-      v-if="useAppStore().isAppSettings(1)"
-      :title="$t('alert.calendarAlert')"
-      close
-      color="neutral"
-      variant="subtle"
-      @update:open="useAppStore().changeAppSettings(1)"
-    />
+  <div class="flex flex-col items-center justify-center gap-2">
+    <UCollapsible
+      v-if="useAppStore().isAppSettings(1)" class="flex flex-col w-full gap-0.5"
+    >
+      <UButton
+        class="group"
+        :label="$t('alert.calendarAlert')"
+        color="neutral"
+        variant="subtle"
+        trailing-icon="i-lucide-chevron-down"
+        :ui="{
+          base: ' py-4',
+          trailingIcon: 'group-data-[state=open]:rotate-180 transition-transform duration-200',
+        }"
+        block
+      />
+
+      <template #content>
+        <UAccordion
+          :items="typesTraining" trailing-icon="ddd" disabled :ui="{
+            trigger: 'opacity-100 py-2.5',
+            root: 'px-2',
+          }"
+        >
+          <template #leading="{ item }">
+            <UIcon v-if="!item.slot" :name="item.icon" class="size-5" :class="item.color" />
+            <UAvatar v-if="item.slot" :class="item.color" icon="i-solar-dumbbell-large-minimalistic-broken" size="lg" />
+          </template>
+        </UAccordion>
+      </template>
+    </UCollapsible>
+
     <div v-if="useActiveStore().isLoadingCalendar" class="fixed inset-0 flex items-center justify-center">
       <LoaderApp />
     </div>
